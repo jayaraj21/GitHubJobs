73ceb77f412349dc8f3fda338f46b9a954f306db
jay z <jayaraj.vis3@gmail.com>
2020-08-09 12:50:51 +0530

    ready to push

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
-import React, {useState} from 'react';
-import './App.css';
-import UseFetchJobs from './UseFetchJobs';
-import Job from './Job';
-import {Container}  from "react-bootstrap"
-import JobPagination from './JobPagination';
+import React, { useState } from 'react';
+import useFetchJobs from './useFetchJobs'
+import { Container } from 'react-bootstrap'
+import Job from './Job'
+import JobsPagination from './JobsPagination';
 import SearchForm from './SearchForm';
 
 function App() {
-  const [params , setParams] = useState({});
-  const  [page, setPage] = useState(1)
-  const {jobs, loading, error} = UseFetchJobs();
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
 
- function  handleParams(e) {
-    const param = e.target.name;
-    const value = e.target.value;
-    setPage(1);
-    setParams(  prevParams => { 
-      return {...prevParams, [param]: value}
-     } )
+  function handleParamChange(e) {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value }
+    })
   }
 
   return (
     <Container className="my-4">
-      <h1>Jay's JOBS</h1>
-      <SearchForm onParamChange = {handleParams()} params={params}/>
-      <JobPagination page={page} setPage={setPage}/>
+      <h1 className="mb-4">Jobs App</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1>Loading...</h1>}
-     
-      {error && <h1>Error... Try Again</h1>}
-      <h1>{jobs.map(job => (
-          <Job key={job.id} job={job}/>
-      ))}</h1>
-      <JobPagination page={page} setPage={setPage}/>
+      {error && <h1>Error. Refresh again.</h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
     </Container>
-  );
+  )
 }
 
 export default App;
--- src/Job.js
+++ src/Job.js
@@ -1,52 +1,43 @@
-import React, {useState} from 'react'
-import {Badge, Button, Card,  Collapse}  from "react-bootstrap"
+import React, { useState } from 'react'
+import { Card, Badge, Button, Collapse } from 'react-bootstrap'
 import ReactMarkdown from 'react-markdown'
-function Job({ job}) {
 
-    const [open, setOpen] = useState(false)
-    return (
-        <Card className="mb-3 "  >
-             <Card.Body>
-                 <div className="d-flex justify-content-between"> 
-                 <div>
-                     
-                     <Card.Title>
-                     {job.title} <span className="text-muted"> --
-                         {job.company}</span> 
-                     </Card.Title>
-                     
-                     <Card.Subtitle className="text-muted mb-2">
-                      {new Date(job.created_at).toLocaleDateString()}
-                      <Badge variant='secondary' className ="m-3">{job.type}</Badge>
-                     <Badge variant='secondary' >{job.location}</Badge>
-                     <div style ={{wordBreak: 'break-all'}}>
-                    <ReactMarkdown source={job.how_to_apply}/>
-                    </div>
-                     </Card.Subtitle>
-                     
-                     <div>
-                        <img alt={job.company} 
-                        src={job.company_logo}  height="50"/>  
-                     </div>
-                    
-                 </div>
-                 
-                 </div>
-               
-            
-            <Card.Text>
-                <Button onClick={() => setOpen(prevOpen => !prevOpen)} 
-                variant="primary" >{open ? "Hide": "View More"}</Button>
-            </Card.Text>
-            <Collapse in={open}>
-            <div className="mt-4">
-                <ReactMarkdown source={job.description}/>
+export default function Job({ job }) {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <Card className="mb-3">
+      <Card.Body>
+        <div className="d-flex justify-content-between">
+          <div>
+            <Card.Title>
+              {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
+            </Card.Title>
+            <Card.Subtitle className="text-muted mb-2">
+              {new Date(job.created_at).toLocaleDateString()}
+            </Card.Subtitle>
+            <Badge variant="secondary" className="mr-2">{job.type}</Badge>
+            <Badge variant="secondary">{job.location}</Badge>
+            <div style={{ wordBreak: 'break-all' }}>
+              <ReactMarkdown source={job.how_to_apply} />
             </div>
-            </Collapse>
-             </Card.Body>
-          
-        </Card>
-    )
+          </div>
+          <img className="d-none d-md-block" height="50" alt={job.company} src={job.company_logo} />
+        </div>
+        <Card.Text>
+          <Button
+            onClick={() => setOpen(prevOpen => !prevOpen)}
+            variant="primary"
+          >
+            {open ? 'Hide Details' : 'View Details'}
+          </Button>
+        </Card.Text>
+        <Collapse in={open}>
+          <div className="mt-4">
+            <ReactMarkdown source={job.description} />
+          </div>
+        </Collapse>
+      </Card.Body>
+    </Card>
+  )
 }
-
-export default Job  
